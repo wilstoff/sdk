33632b1c6b29a157b56ff46166617bb3ab4d8b1f
Daniel Plaisted <daplaist@microsoft.com>
2021-12-28 19:15:40 -0800

    Add more debug output to running helix and non-helix tests in parallel

--- eng/runHelixAndNonHelixInParallel.ps1
+++ eng/runHelixAndNonHelixInParallel.ps1
@@ -21,7 +21,34 @@
             [string] $engfolderPath
         )
 
+        parallel {
+            RunTestsCannotRunOnHelix -configuration $configuration -engfolderPath $engfolderPath
+            RunTestsOnHelix -configuration $configuration -buildSourcesDirectory $buildSourcesDirectory -customHelixTargetQueue $customHelixTargetQueue -engfolderPath $engfolderPath
+        }
+    }
+
+    runHelixAndNonHelixInParallel -configuration $configuration -buildSourcesDirectory $buildSourcesDirectory -customHelixTargetQueue $customHelixTargetQueue -engfolderPath $PSScriptRoot
+
+    function RunTestsCannotRunOnHelix(
+        [string] $configuration,
+        [string] $engfolderPath
+    )
+    {
+        Write-Host "Running tests that can't run on Helix..."
         $runTestsCannotRunOnHelixArgs = ("-configuration", $configuration, "-ci")
+        Invoke-Expression "&'$engfolderPath\runTestsCannotRunOnHelix.ps1' $runTestsCannotRunOnHelixArgs"
+        Write-Host "Done running tests that can't run on Helix..."
+    }
+
+    function RunTestsOnHelix(
+        [string] $configuration,
+        [string] $buildSourcesDirectory,
+        [string] $customHelixTargetQueue,
+        [string] $engfolderPath
+    )
+    {
+        Write-Host "Running tests in Helix..."
+
         $runTestsOnHelixArgs = ("-configuration", $configuration,
         "-prepareMachine",
         "-ci",
@@ -31,10 +58,7 @@
         "/bl:$buildSourcesDirectory\artifacts\log\$configuration\TestInHelix.binlog",
         "/p:_CustomHelixTargetQueue=$customHelixTargetQueue")
 
-        parallel {
-            Invoke-Expression "&'$engfolderPath\runTestsCannotRunOnHelix.ps1' $runTestsCannotRunOnHelixArgs"
-            Invoke-Expression "&'$engfolderPath\common\build.ps1' $runTestsOnHelixArgs"
-        }
-    }
+        Invoke-Expression "&'$engfolderPath\common\build.ps1' $runTestsOnHelixArgs"
 
-    runHelixAndNonHelixInParallel -configuration $configuration -buildSourcesDirectory $buildSourcesDirectory -customHelixTargetQueue $customHelixTargetQueue -engfolderPath $PSScriptRoot
+        Write-Host "Done running tests on Helix..."
+    }
